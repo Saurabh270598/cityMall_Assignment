4286ad1a9c2d452f74816a5a6d6def5ed044b692
saurabh singh <81359361+Saurabh270598@users.noreply.github.com>
2021-10-23 13:58:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css";
+import Table from './components/table';
+
+
+export default function App() {
+  return (
+     <div>
+    <Table/>
+    
+    </div>
+  );
+}
